f3c9a90fedd66a525da08ee985fbb4e3c199a7b4
rsonaje <sonaje.rohit@gmail.com>
2021-10-26 20:53:11 +0530

    Base NuGet package project to create NuGet pacakge on Azure Repo

---
+++ NuGetBaseProject/BaseLibrary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NuGetBaseProject
+{
+    public class BaseLibrary
+    {
+        public string VersionInfo
+        {
+            get { return "This is default version: 1.0.0"; }
+        }
+
+        private string message;
+        public string Message
+        {
+            get
+            {
+                return message;
+            }
+            set
+            {
+                message = value;
+            }
+        }
+    }
+}
